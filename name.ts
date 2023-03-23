52160fc7eb739e8f11a3664484ec014484f76452
baxit2894 <baxit2894@gmail.com>
2023-03-23 20:38:24 +0500

    name001

---
+++ name.ts
@@ -0,0 +1,15 @@
+// //Template Litrels\\
+
+              //with comas\\
+// let name1 = "Abdul BASIT";
+// let name2 = "ALI HASSAN";
+// let name3 = "SALMAN ALI";
+// let friends1 = `${name1}, ${name2}, ${name3}`;
+// console.log (friends);
+
+                //with new lines and type\\
+let name1 = "Abdul BASIT";
+let name2 = "ALI HASSAN";
+let name3 = "SALMAN ALI";
+let friends1 = `${name1}\n${name2}\n${name3}\n`;
+console.log (friends1,typeof friends1);
